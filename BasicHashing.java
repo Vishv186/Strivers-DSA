e69bd3da908e5c5f5c9f7c1e89e95e9040eab10b
Vishv Nagde <121821324+Vishv186@users.noreply.github.com>
2025-10-06 11:16:11 +0530

    Create BasicHashing.java

---
+++ BasicHashing.java
@@ -0,0 +1,30 @@
+/*
+Hashing :- The combination of the steps, pre-storing, and fetching. 
+*/
+
+public class BasicHashing {
+    static void countFreq(int arr[], int n) {
+        boolean visited[] = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == true) {
+                continue;  
+            }
+
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " " + count);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        countFreq(arr, n);
+    }
+}
