b9ee80b627fd77aa99335d77609cc44af110673a
Vishv Nagde <121821324+Vishv186@users.noreply.github.com>
2025-10-06 10:18:09 +0530

    Update BasicRecursion.java

--- BasicRecursion.java
+++ BasicRecursion.java
@@ -193,3 +193,32 @@ Check if the given String is Palindrome or not
 //        System.out.println(palindrome(0,s));
 //     }
 // }
+
+
+
+
+
+/*
+Fibonacci Number
+Given an integer N. Print the Fibonacci series up to the Nth term.
+*/
+
+// public class BasicRecursion {
+//   public static void main(String args[]) {
+//     int n = 5;
+//     if (n == 0) {
+//       System.out.println(0);
+//     } else {
+//       int fib[] = new int[n + 1];
+//       fib[0] = 0;
+//       fib[1] = 1;
+//       for (int i = 2; i <= n; i++) {
+//         fib[i] = fib[i - 1] + fib[i - 2];
+//       }
+//       System.out.println("The Fibonacci Series up to "+n+"th term:");
+//       for (int i = 0; i <= n; i++) {
+//         System.out.print(fib[i] + " ");
+//       }
+//     }
+//   }
+// }
